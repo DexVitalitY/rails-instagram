b301a8a912cdbee0f810c644942d5d7f2d146164
Michael Chow <DexVitalitY@Michaels-MacBook-Pro.local>
2015-05-06 15:20:18 +0800

    First Commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,14 @@
+class StaticPagesController < ApplicationController
+	#We always want def index here, convention to have it.
+	def index
+		#No logics needed. Leave Empty
+
+# numbers = [1,2,3,4]
+# 		render :json => {
+# 			person: {
+# 				name: "michael"
+# 			},
+# 			lucky_numbers: numbers
+# 		}
+	end
+end
